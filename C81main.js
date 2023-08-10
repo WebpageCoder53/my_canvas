09b2df7fd2c0fa5f0eb1e2cbf596dcdaf03e04d6
WebpageCoder53 <133002197+WebpageCoder53@users.noreply.github.com>
2023-08-10 17:37:44 +0530

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -9,15 +9,46 @@ ctx.arc(200, 200, 40, 0, 2*Math.PI);
 ctx.stroke();
 
 canvas.addEventListener("mousedown", my_mousedown);
-
+canvas.addEventListener("mouseleave", my_mouseleave);
+canvas.addEventListener("mouseup", my_mouseup);
+canvas.addEventListener("mousemove", my_mousemove);
 
 function my_mousedown(e) {
-    mouse_x=e.clientX - canvas.offsetLeft;
-    mouse_y=e.clientY - canvas.offsetTop;
-    console.log(mouse_x, mouse_y);
-    circle(mouse_x, mouse_y);
+    color=document.getElementById("color").value;
+    width_of_line=document.getElementById("width_of_line").value;
+
+    mouseEvent="mousedown";
 }
 
+function my_mouseleave(e) {
+    mouseEvent="mouseleave";
+}
+
+function my_mouseup(e) {
+    mouseEvent="mouseup";
+}
+
+function my_mousemove(e) {
+    current_mouseX = e.clientX - canvas.offsetLeft;
+    current_mouseY = e.clientY - canvas.offsetTop;
+    
+    if (mouseEvent == "mousedown") {
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+
+        console.log("Last position of x and y:");
+        console.log(last_mouseX + ", " + last_mouseY);
+        ctx.moveTo(last_mouseX, last_mouseY);
+
+        console.log("Current position of x and y:");
+        console.log(current_mouseX + ", " + current_mouseY);
+        ctx.lineTo(current_mouseX, current_mouseY);
+        ctx.stroke();
+    }
+    last_mouseX = current_mouseX;
+    last_mouseY = current_mouseY;
+}
 
 
 function circle(mouse_x, mouse_y) {
